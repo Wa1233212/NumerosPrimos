3d2d678de3a2721fc1a4f129639947d725e97759
Wa1233212 <carlos.c.c.b.m@gmail.com>
2023-02-14 17:53:20 +0100

    Version 1.1 Eliminada variable innecesaria dim.

--- src/numerosprimos/CribaEratostenes.java
+++ src/numerosprimos/CribaEratostenes.java
@@ -34,34 +34,33 @@ public class CribaEratostenes
  if (max >= 2) { 
  
  // Declaraciones
- int dim = max + 1; // Tamaño del array 
- boolean[] esPrimo = new boolean[dim]; 
+ boolean[] esPrimo = new boolean[max + 1]; 
  
  // Inicializar el array
- for (i=0; i<dim; i++) 
+ for (i=0; i<esPrimo.length; i++) 
  esPrimo[i] = true; 
  
  // Eliminar el 0 y el 1, que no son primos
  esPrimo[0] = esPrimo[1] = false; 
  
  // Criba
- for (i=2; i<Math.sqrt(dim)+1; i++) { 
- if (esPrimo[i]) {
- // Eliminar los múltiplos de i
- for (j=2*i; j<dim; j+=i) 
- esPrimo[j] = false; 
- } 
- }
+ for (i=2; i<Math.sqrt(esPrimo.length)+1; i++) { 
+    if (esPrimo[i]) {
+    // Eliminar los múltiplos de i
+       for (j=2*i; j<esPrimo.length; j+=i) 
+          esPrimo[j] = false; 
+       } 
+}
  
  // ¿Cuántos primos hay? 
  int cuenta = 0; 
- for (i=0; i<dim; i++) { 
+ for (i=0; i<esPrimo.length; i++) { 
  if (esPrimo[i]) 
  cuenta++; 
  } 
  // Rellenar el vector de números primos
  int[] primos = new int[cuenta]; 
- for (i=0, j=0; i<dim; i++) { 
+ for (i=0, j=0; i<esPrimo.length; i++) { 
  if (esPrimo[i])
  primos[j++] = i; 
  } 
